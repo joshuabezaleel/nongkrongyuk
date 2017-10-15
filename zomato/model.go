9d3d8edb963da1849b1823dfb0ff2357378d4575
Ahmad Zaky <ahmadzaky003@gmail.com>
2017-10-15 18:24:10 +0700

    Fix json response in search restaurants

--- zomato/model.go
+++ zomato/model.go
@@ -1,5 +1,9 @@
 package zomato
 
+import (
+	"encoding/json"
+)
+
 //Location is rad as fuck
 type Location struct {
 	Address   string `json:"address"`
@@ -35,8 +39,33 @@ type SearchCityIDResponse struct {
 
 //SearchByLatLongResponse is rad as fuck
 type SearchByLatLongResponse struct {
-	NTotalResult int           `json:"results_found"`
-	Offset       int           `json:"results_start"`
-	NResult      int           `json:"results_shown"`
-	Restaurants  []*Restaurant `json:"restaurants"`
+	NTotalResult int
+	Offset       int
+	NResult      int
+	Restaurants  []*Restaurant
+}
+
+type searchByLatLongResponseFormat struct {
+	NTotalResult int `json:"results_found"`
+	Offset       int `json:"results_start"`
+	NResult      int `json:"results_shown"`
+	Restaurants  []struct {
+		Restaurant *Restaurant `json:"restaurant"`
+	} `json:"restaurants"`
+}
+
+func (r *SearchByLatLongResponse) JSONUnmarshal(data []byte) error {
+	var formatted searchByLatLongResponseFormat
+	err := json.Unmarshal(data, &formatted)
+	if err != nil {
+		return err
+	}
+	r.NTotalResult = formatted.NTotalResult
+	r.Offset = formatted.Offset
+	r.NResult = formatted.NResult
+	r.Restaurants = make([]*Restaurant, 0)
+	for _, restaurant := range formatted.Restaurants {
+		r.Restaurants = append(r.Restaurants, restaurant.Restaurant)
+	}
+	return nil
 }
