ebb1e48c2ef851fdbdc20065a6436f3eccbfa8bf
joshuabezaleel <joshua.bezaleel@gmail.com>
2017-10-15 16:23:59 +0700

    try carousel template action

--- main.go
+++ main.go
@@ -88,7 +88,12 @@ func main() {
 					// 		log.Print(err)
 					// 	}
 					// }
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("a")).Do(); err != nil {
+					imageURL := "https://user-images.githubusercontent.com/7043511/31583356-630ca11c-b1c4-11e7-8109-16228f8a5c0b.png"
+					template := linebot.NewCarouselTemplate(
+						linebot.NewCarouselColumn(imageURL, "title carousel", "text carousel",
+							linebot.NewMessageTemplateAction("label message template action", "text message template action"))
+					)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTemplateMessage("carousel alt text", template).Do(); err != nil {
 						log.Print(err)
 					}
 				case *linebot.LocationMessage:
